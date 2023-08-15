b44a84a8f8f466f73e1a0952a869934556cf94f0
Yudi <yudi.mukhyiddin@ecampus.ut.ac.id>
2023-08-15 16:00:59 +0700

    add tailwind css

--- app/tes/page.js
+++ app/tes/page.js
@@ -1,10 +1,41 @@
+'use client';
+
 import Image from 'next/image'
 import styles from '../page.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
 
 export default function tes() {
+  const [getSum,setSum]= useState(1)
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  // const a = setSum('s')
+
+  const abc = () =>{
+    setSum(getSum + 10)
+  }
+
+  // useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=670c2dba');
+        setData(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      }
+    }
+
+    // fetchData();
+  // }, []);
+
+  console.log('sdw',data);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div >
         <p>
           &nbsp;
           <code className={styles.code}>app/page.js</code>
@@ -14,6 +45,9 @@ export default function tes() {
             href="/"
             rel="noopener noreferrer"
           >
+            <h1 className='mt-10'>
+              SDWD
+            </h1>
             By{' '}
             <Image
               src="/vercel.svg"
@@ -25,6 +59,12 @@ export default function tes() {
             />
           </a>
         </div>
+      </div>
+      <div>
+        <button onClick={abc}>
+          click me !!
+        </button>
+        ___ {getSum}
       </div>
     </main>
   )
